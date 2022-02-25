95b679489696bd0ef13c2b1569b880e0a62506ab
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-02-24 21:14:04 -0600

    spring boot with JPA

--- src/main/java/com/springboot/CRUDdemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/CRUDdemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.springboot.CRUDdemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +15,15 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	private EmployeeDAO employeeDAO;
 
+	/*Parameter 0 of constructor in com.springboot.CRUDdemo.service.EmployeeServiceImpl required a single bean, but 2 were found:
+	 *- employeeDAOHibernateImpl: and - employeeDAOJpaImpl:
+	 * Consider marking one of the beans as @Primary, updating the consumer to accept multiple beans, or using @Qualifier to identify the bean that should be consumed
+	 * 
+	 * Added @Qualifier annotation with lower case class name
+	 */
+	
 	@Autowired
-	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
 		employeeDAO = theEmployeeDAO;
 	}
 
